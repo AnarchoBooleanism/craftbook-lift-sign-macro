5629232fe2ec6efd0a96387c97ade5b9842e9e08
Nathan Guerrero <naguerr2@uci.edu>
2022-04-26 00:46:14 -0700

    Finished program functionality

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from sign_generator import generate_sign
 from get_input import get_bool_input, get_int_input
 import keyboard
 
+HOTKEY = "F4"
+
 while __name__ == "__main__":
+    print("Welcome to the CraftBook Lift Sign Macro program!")
+
     floor_count = get_int_input( # Get various arguments
         prompt_text="Enter a number of floors",
         is_legal=(lambda x: x > 0),
@@ -12,6 +16,16 @@ while __name__ == "__main__":
     )
     has_basement = get_bool_input(prompt_text="Enter whether to include the basement as a floor", default=False)
     has_rooftop = get_bool_input(prompt_text="Enter whether to include the rooftop as a floor", default=False)
+
+    print(f"To write a sign, press \"{HOTKEY}\".")
+
+    for text in generate_sign(floor_count, has_basement, has_rooftop): # Get generator, and iterate over each list
+        keyboard.wait(HOTKEY) # Wait for hotkey, then type text and escape
+        print(f"Writing \"{text[0]}\"...")
+        for line in text:
+            keyboard.write(line)
+            keyboard.send("enter")
+        keyboard.send("esc")
 
     if not get_bool_input(prompt_text="Would you like to create another elevator?"): # Confirm whether to loop again
         break
